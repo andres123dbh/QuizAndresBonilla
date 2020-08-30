aac34d1c98de11b73a919c0c21cc2d6bfeb34803
andres123dbh <60190072+andres123dbh@users.noreply.github.com>
2020-08-29 22:52:43 -0500

    Formulario inicial del laboratorio

---
+++ Home.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuizAndresBonilla
+{
+    /// <summary>
+    /// Lógica de interacción para Home.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click_1(object sender, RoutedEventArgs e)
+        {
+            string animeName = txtAnimeName.Text;
+            string category = lblCategory.Content.ToString();
+            txtResults.Text = category+"\n"+animeName;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            if (rb.IsChecked.Value)
+            {
+                lblCategory.Content = rb.Content.ToString();
+            }
+        }
+    }
+}
